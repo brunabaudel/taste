7be56a399447ff383c9abcede187d71b5faacb66
brunabaudel <brubaudel@gmail.com>
2017-08-20 17:59:04 -0300

    Adding indices and foreign keys.

--- taste.sql
+++ taste.sql
@@ -10,7 +10,10 @@ CREATE TABLE `comments` (
   `location` varchar(100) NOT NULL DEFAULT '',
   `comment` longtext NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`comment_id`)
+  PRIMARY KEY (`comment_id`),
+  KEY `post_id` (`post_id`),
+  KEY `post_user_id` (`post_user_id`),
+  KEY `comment_of_user_id` (`comment_of_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -26,7 +29,10 @@ CREATE TABLE `favorites` (
   `status` enum('active','banned') NOT NULL DEFAULT 'active',
   `location` varchar(100) NOT NULL DEFAULT '',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`favorite_id`)
+  PRIMARY KEY (`favorite_id`),
+  KEY `post_id` (`post_id`),
+  KEY `post_user_id` (`post_user_id`),
+  KEY `favorite_of_user_id` (`favorite_of_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -43,7 +49,8 @@ CREATE TABLE `posts` (
   `status` enum('active','banned') NOT NULL DEFAULT 'active',
   `image_path` varchar(30) NOT NULL DEFAULT '',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`post_id`)
+  PRIMARY KEY (`post_id`),
+  KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -61,7 +68,10 @@ CREATE TABLE `ratings` (
   `status` enum('active','banned') NOT NULL DEFAULT 'active',
   `location` varchar(100) NOT NULL DEFAULT '',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`rating_id`)
+  PRIMARY KEY (`rating_id`),
+  KEY `post_id` (`post_id`),
+  KEY `post_user_id` (`post_user_id`),
+  KEY `rate_of_user_id` (`rate_of_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -77,7 +87,10 @@ CREATE TABLE `users` (
   `profile_image_path` varchar(30) NOT NULL DEFAULT '',
   `status` enum('active','banned') NOT NULL DEFAULT 'active',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`)
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `facebook_id` (`facebook_id`),
+  UNIQUE KEY `username` (`username`),
+  KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -89,7 +102,10 @@ CREATE TABLE `views` (
   `view_id` char(36) NOT NULL DEFAULT '',
   `post_id` char(36) NOT NULL DEFAULT '',
   `user_id` char(36) NOT NULL DEFAULT '',
-  `view_of_user_id` char(36) NOT NULL DEFAULT '',
+  `view_of_user_id` char(36) DEFAULT '',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`view_id`)
+  PRIMARY KEY (`view_id`),
+  KEY `post_id` (`post_id`),
+  KEY `user_id` (`user_id`),
+  KEY `view_of_user_id` (`view_of_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
